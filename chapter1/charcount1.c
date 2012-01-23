6db56c1f745da26feab4e434c2109404cc359499
gvnn <gvnnrulez@gmail.com>
2012-01-23 22:48:28 +1100

    Section 1.5.2 completed :)

---
+++ chapter1/charcount1.c
@@ -0,0 +1,14 @@
+/* section 1.5.2 */
+#include <stdio.h>
+
+/* count characters in input; 1st version */
+main()
+{
+    long nc;
+    
+    nc = 0;
+    while (getchar() != EOF)
+        ++nc;
+        
+    printf("%ld\n", nc);
+}
