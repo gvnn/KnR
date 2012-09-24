52144eb4a51e143668001721e57eefe6d198ca85
gvnn <gvnnrulez!>
2012-09-24 23:21:54 +1000

    we don't like tabs!

--- chapter1/1.5/1.5.3/linescount.c
+++ chapter1/1.5/1.5.3/linescount.c
@@ -3,10 +3,10 @@
 /* count lines in input */
 int main()
 {
-	int c, nl;
-	nl = 0;
-	while((c = getchar()) != EOF)
-		if (c == '\n')
-			++nl;
-	printf("%d\n", nl);
+    int c, nl;
+    nl = 0;
+    while((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
 }
